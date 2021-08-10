38fd7c5aec8e21e3434737f6028db652f9adf8bf
Marko Savic <marko.savic@homegate.ch>
2021-08-10 16:06:40 +0200

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var carsRouter = require('./routes/cars');
 
@@ -11,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/cars', carsRouter);
 
