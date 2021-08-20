8978a6a4894ce46137a051305cf89452d9fda550
Marko Savic <marko.savic@homegate.ch>
2021-08-20 15:32:15 +0200

    add create service form

--- webapp/carshistory/src/views/singlecar/SingleCar.js
+++ webapp/carshistory/src/views/singlecar/SingleCar.js
@@ -5,19 +5,48 @@ import {
   useParams
 } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import {Carousel} from 'react-responsive-carousel';
 
 function SingleCar() {
   const {carId} = useParams();
-  const [carData, setCarDara] = useState({images:[], serviceHistory: []});
+  const [carData, setCarDara] = useState({images: [], serviceHistory: []});
+  const [working, setWorking] = useState(false);
+  const [serviceHistoryDate, setServiceHistoryDate] = useState(Date.now());
+  const [serviceHistoryMileage, setServiceHistoryMileage] = useState(0);
+  const [serviceHistoryType, setServiceHistoryType] = useState('INTERIM_SERVICE');
+  const [serviceHistoryDescription, setServiceHistoryDescription] = useState('');
 
   useEffect(async () => {
-    const result = await axios(
-      `http://localhost:5000/cars/${carId}`,
-    );
+    if(!working) {
+      const result = await axios(
+        `http://localhost:5000/cars/${carId}`,
+      );
 
-    setCarDara(result.data);
-  }, []);
+      setCarDara(result.data);
+    }
+  }, [working]);
+
+  const submitHandler = async (event) => {
+    setWorking(true);
+    event.preventDefault();
+    console.log({serviceHistoryDate, serviceHistoryMilege: serviceHistoryMileage, serviceHistoryType, serviceHistoryDescription});
+    const result = await axios.post('http://localhost:5000/cars/services',
+      {
+        date: serviceHistoryDate,
+        mileage: serviceHistoryMileage,
+        serviceType: serviceHistoryType,
+        description: serviceHistoryDescription,
+        vin: carData.vin
+      });
+
+
+    setServiceHistoryMileage(0);
+    setServiceHistoryDate(Date.now());
+    setServiceHistoryType('INTERIM_SERVICE');
+    setServiceHistoryDescription('');
+    setWorking(false)
+    console.log(result);
+  };
 
   return (
     <div>
@@ -25,7 +54,7 @@ function SingleCar() {
       <Carousel className="appCarousel">
         {carData.images.map(imageUrl => (
           <div>
-            <img src={imageUrl}  alt=""/>
+            <img src={imageUrl} alt=""/>
           </div>
         ))}
       </Carousel>
@@ -54,9 +83,54 @@ function SingleCar() {
               <p className="mainDataItem">Service type: {historyEntry.serviceType}</p>
               <p className="mainDataItem">Mileage: {historyEntry.mileage}</p>
               <p className="mainDataItem">Date: {historyEntry.date}</p>
-              <p className="mainDataItem">Date: {historyEntry.description}</p>
+              <p className="mainDataItem">Description: {historyEntry.description}</p>
             </div>
           ))}
+        </details>
+      </div>
+
+      <div>
+        <details>
+          <summary className="subtitle">Add Service Data</summary>
+          <form className="serviceHistoryForm" onSubmit={submitHandler}>
+            <label className="formField">
+              Date
+              <input type="date"
+                     value={serviceHistoryDate}
+                     onChange={e => setServiceHistoryDate(e.target.value)}
+                     placeholder="Enter date..."
+                     required/>
+            </label>
+
+            <label className="formField">
+              Mileage
+              <input type="text"
+                     value={serviceHistoryMileage}
+                     onChange={e => setServiceHistoryMileage(e.target.value)}
+                     required/>
+            </label>
+
+            <label className="formField">
+              Service type
+              <select id="lang" onChange={e => setServiceHistoryType(e.target.value)} value={serviceHistoryType}>
+                <option value="INTERIM_SERVICE">Interim service</option>
+                <option value="FULL_SERVICE">Full service</option>
+                <option value="AC_SERVICE">Air condition service</option>
+                <option value="REPAIR">Repair (other)</option>
+              </select>
+            </label>
+
+            <label className="formField">
+              Description
+              <textarea className="descriptionField"
+                        value={serviceHistoryDescription}
+                        onChange={e => setServiceHistoryDescription(e.target.value)}
+                        placeholder="Enter placeholder..."
+                        required/>
+            </label>
+
+            <input className="submitButton" type="submit" value="Create"/>
+          </form>
         </details>
       </div>
     </div>
